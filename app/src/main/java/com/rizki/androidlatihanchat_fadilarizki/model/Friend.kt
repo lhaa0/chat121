5a67f17e08d9561a69d2295ef09ebb82228083c9
lhaa0 <riski.rama54@gmail.com>
2019-07-11 16:25:27 +0700

    chat

---
+++ app/src/main/java/com/rizki/androidlatihanchat_fadilarizki/model/Friend.kt
@@ -0,0 +1,6 @@
+package com.rizki.androidlatihanchat_fadilarizki.model
+
+import java.io.Serializable
+
+
+class Friend(val id: String, val name: String, val photo: String) : Serializable
---
+++ app/src/main/java/com/rizki/androidlatihanchat_fadilarizki/services/FirebaseMsgServices.kt
@@ -0,0 +1,15 @@
+package com.rizki.androidlatihanchat_fadilarizki.services
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.rizki.androidlatihanchat_fadilarizki.utilities.Const.Companion.LOG_TAG
+
+class FirebaseMsgServices : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+        Log.d(LOG_TAG, "FCM Message Id: " + p0!!.getMessageId()!!)
+        Log.d(LOG_TAG, "FCM Notification Message: " + p0!!.getNotification()!!)
+        Log.d(LOG_TAG, "FCM Data Message: " + p0!!.getData())
+    }
+}
